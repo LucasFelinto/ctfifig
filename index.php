e4b7230c81f1b409534b87f6398508d2b790e100
LucasFelinto <elucasfelinto@gmail.com>
2020-06-17 13:18:14 -0300

    inital commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/base.css">
+  <title>CTF IFIG</title>
+</head>
+<body>
+  <h1>CTF IFIG</h1>
+
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, excepturi tenetur enim amet non natus fugit, debitis optio vel dolore odit laudantium, at vitae nesciunt quisquam molestiae nostrum nihil nobis.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorum, esse, quo maxime laborum perspiciatis ipsa ipsum numquam obcaecati rerum aut earum impedit ducimus id natus doloremque saepe, eveniet alias.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sit eligendi ipsa fugit error sapiente animi adipisci non cumque? Pariatur amet quia rem adipisci ratione optio ad dolorem eveniet sunt.
+  </p>
+</body>
+</html>
