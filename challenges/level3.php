b7ddef5049b748c14305a2c036faacd33ba43dbf
Khevin <Karimoxiha@gmail.com>
2020-06-17 16:30:34 -0300

    Level 3 incompleto

---
+++ challenges/level3.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../css/base.css">
+  <link rel="stylesheet" href="../css/post.css">
+  <link rel="stylesheet" href="../css/header.css">
+  <title>CTF IFIG</title>
+</head>
+<body>
+  <?php
+    include '../components/header.php'; 
+  setcookie("ZipFile", "Y29va2llcwo=");
+  ?>
+
+  <section class="post">
+    <img src="../img/level3.jpg"/>
+    <!-- COOKIE! -->
+  </section>
+</body>
+</html>
--- components/header.php
+++ components/header.php
@@ -4,7 +4,7 @@
     <nav>
       <a href="/challenges/level1.php"> Level 1</a>
       <a href="/challenges/level2.php">Level 2</a>
-      <a href="">Level 3</a>
+      <a href="/challenges/level3.php">Level 3</a>
       <a href="">Level 4</a>
       <a href="">Level 5</a>
       <a href="">Level 6</a>
