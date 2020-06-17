8c4b95c54023a021e5e28e2596ea68354821314e
Khevin <Karimoxiha@gmail.com>
2020-06-17 15:25:35 -0300

    Level 2 adicionado

---
+++ challenges/level2.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../css/base.css">
+  <link rel="stylesheet" href="../css/post.css">
+  <link rel="stylesheet" href="../css/header.css">
+  <title>CTF IFIG</title>
+</head>
+<body>
+  <?php
+    include '../components/header.php';
+  ?>
+
+  <section class="post">
+    <img src="../img/level2.png"/>
+    <h1 class="ch">Normalmente os homens preocupam-se mais com aquilo que não podem ver do que com aquilo que podem.</h1>
+  </section>
+</body>
+</html>
