5f16585432f8adf02e22c484d77e3039fdd353dc
LucasFelinto <elucasfelinto@gmail.com>
2020-06-17 14:56:34 -0300

    home page

--- index.php
+++ index.php
@@ -3,17 +3,46 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/base.css">
+  <link rel="stylesheet" href="css/post.css">
+  <link rel="stylesheet" href="css/header.css">
   <title>CTF IFIG</title>
 </head>
 <body>
-  <h1>CTF IFIG</h1>
+  <header class="header">
+    <h1>CTF IFIG</h1>
+    
+    <nav>
+      <a href="">Level 1</a>
+      <a href="">Level 2</a>
+      <a href="">Level 3</a>
+      <a href="">Level 4</a>
+      <a href="">Level 5</a>
+      <a href="">Level 6</a>
+      <a href="">Level 7</a>
+    </nav>
+  </header>
 
-  <p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, excepturi tenetur enim amet non natus fugit, debitis optio vel dolore odit laudantium, at vitae nesciunt quisquam molestiae nostrum nihil nobis.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolorum, esse, quo maxime laborum perspiciatis ipsa ipsum numquam obcaecati rerum aut earum impedit ducimus id natus doloremque saepe, eveniet alias.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sit eligendi ipsa fugit error sapiente animi adipisci non cumque? Pariatur amet quia rem adipisci ratione optio ad dolorem eveniet sunt.
-  </p>
+  <section class="post">
+    <img src="https://cdn1.iconfinder.com/data/icons/game-design-color-pop-vol-1/64/capture-the-flag-512.png" alt="capture the flag">
+    <p>
+  
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ea mollitia exercitationem a culpa repellat sed aperiam laborum! Recusandae consectetur sit ipsum at tenetur laboriosam maxime in et magnam illum!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa nihil porro, fuga possimus amet odit pariatur in iure libero ut quibusdam labore quasi, ipsum sapiente delectus reprehenderit debitis, eveniet est?
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur tempore assumenda porro, laborum eligendi exercitationem dolores? Itaque libero exercitationem vero officiis mollitia distinctio voluptates facere veritatis corporis, nobis quam neque!
+    </p>
+
+
+    <h1>Ferramentas</h1>
+
+    <ul>
+      <li>Ferramenta 1</li>
+      <li>Ferramenta 2</li>
+      <li>Ferramenta 3</li>
+      <li>Ferramenta 4</li>
+      <li>Ferramenta 5</li>
+    </ul>
+  </section>
 </body>
 </html>
