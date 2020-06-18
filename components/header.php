e31553e70bea3f9bb2a60740d2d14d7af7c11e95
Khevin <Karimoxiha@gmail.com>
2020-06-17 21:48:05 -0300

    cookie

--- components/header.php
+++ components/header.php
@@ -3,8 +3,8 @@
     
     <nav>
       <a href="/challenges/level1.php"> Level 1</a>
-      <a href="/challenges/level2.php">Level 2</a>
-      <a href="/challenges/level3.php">Level 3</a>
+      <a href="/challenges/level2.php"> Level 2</a>
+      <a href="/challenges/level3.php"> Level 3</a>
       <a href="">Level 4</a>
       <a href="">Level 5</a>
       <a href="">Level 6</a>
